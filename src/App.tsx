fe7e9f2c2fc88d042351a36efdfa1a6ad722c99b
Nama-Salameh <nama.salameh@gmail.com>
2024-03-21 11:33:04 +0200

    Delete unnecessary imports.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import React from "react";
 import "./App.scss";
-import OppositeDisplay from "./OppositeDisplayTask/OppositeDisplay";
-import data from "./constants/data";
 import NatureListManger from "./NatureTask/natureListManager/NatureListManger";
 
 function App() {
