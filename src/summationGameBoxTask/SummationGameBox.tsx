de8ee8518551b857ca1575c01ad8bfa2190bd07d
Nama-Salameh <nama.salameh@gmail.com>
2024-04-08 14:10:22 +0300

    Refactor summation game box task.

--- src/summationGameBoxTask/SummationGameBox.tsx
+++ src/summationGameBoxTask/SummationGameBox.tsx
@@ -21,10 +21,7 @@ const SummationGameBox: React.FC = () => {
     wrong: number;
   }>({ roundCounter: 1, correct: 0, wrong: 0 });
   const [message, setMessage] = useState<string>("");
-  const [resetButtonDisplayed, setResetButtonDisplayed] =
-    useState<boolean>(false);
   const [equations, setEquations] = useState<equation[]>([]);
-  const [equationsDisplay, setEquationsDisplay] = useState<boolean>(false);
 
   const generateRandomNumber = (
     min: number,
@@ -43,11 +40,16 @@ const SummationGameBox: React.FC = () => {
       generateRandomNumber(1, 20)
     );
     const newResult = newNumbers[0] + newNumbers[1];
-    const firstRandomResult = generateRandomNumber(1, 50, [newResult]);
-    const secondRandomResult = generateRandomNumber(1, 50, [
-      newResult,
-      firstRandomResult,
-    ]);
+    const firstRandomResult = generateRandomNumber(
+      newResult - 3,
+      newResult + 3,
+      [newResult]
+    );
+    const secondRandomResult = generateRandomNumber(
+      newResult - 3,
+      newResult + 3,
+      [newResult, firstRandomResult]
+    );
     setNumbers(newNumbers);
     setResult(newResult);
     setRandomResults([firstRandomResult, secondRandomResult]);
@@ -60,19 +62,28 @@ const SummationGameBox: React.FC = () => {
       [isCorrect ? "correct" : "wrong"]:
         prevCounters[isCorrect ? "correct" : "wrong"] + 1,
     }));
-    setEquations((prevEquations) => [
-      ...prevEquations,
-      {
-        num1: numbers[0],
-        num2: numbers[1],
-        result: result,
-        selectedResult: value,
-        isCorrect: isCorrect,
-      },
-    ]);
-    if (counters.roundCounter !== 5) {
-      setValues();
+
+    if (!isCorrect)
+      setEquations((prevEquations) => [
+        ...prevEquations,
+        {
+          num1: numbers[0],
+          num2: numbers[1],
+          result: result,
+          selectedResult: value,
+          isCorrect: isCorrect,
+        },
+      ]);
+
+    if (counters.roundCounter + 1 > 5) {
+      if (counters.correct + (isCorrect ? 1 : 0) === 5) {
+        setMessage("All guesses are correct!");
+      } else if (counters.wrong !== 0) {
+        setMessage("Try guessing again");
+      }
     }
+
+    setValues();
     setCounters((prevCounters) => ({
       ...prevCounters,
       roundCounter: prevCounters.roundCounter + 1,
@@ -82,7 +93,6 @@ const SummationGameBox: React.FC = () => {
   const handleResetClick = () => {
     setCounters({ roundCounter: 1, correct: 0, wrong: 0 });
     setMessage("");
-    setResetButtonDisplayed(false);
     setEquations([]);
     setValues();
   };
@@ -91,74 +101,71 @@ const SummationGameBox: React.FC = () => {
     setValues();
   }, []);
 
-  useEffect(() => {
-    if (counters.roundCounter > 5) {
-      if (counters.correct === 5) {
-        setMessage("All guesses are correct!");
-        setResetButtonDisplayed(true);
-      } else if (counters.wrong !== 0) {
-        setMessage("Try guessing again");
-        setEquationsDisplay(true);
-      }
-    }
-  }, [counters]);
-
   return (
     <div className="summationGameBoxContainer">
-      <div className="equationContainer">
-        {numbers[0]} + {numbers[1]}
-      </div>
-      <div className="resultButtonsContainer">
-        {[result, ...randomResults]
-          .sort(() => Math.random() - 0.5)
-          .map((value, index) => (
-            <button
-              key={index}
-              className="resultButton button"
-              onClick={() => handleClick(value)}
-            >
-              {value}
+      {counters.roundCounter <= 5 && (
+        <div className="container">
+          <div className="equationContainer">
+            {numbers[0]} + {numbers[1]}
+          </div>
+          <div className="resultButtonsContainer">
+            {[result, ...randomResults]
+              .sort(() => Math.random() - 0.5)
+              .map((value, index) => (
+                <button
+                  key={index}
+                  className="resultButton button"
+                  onClick={() => handleClick(value)}
+                >
+                  {value}
+                </button>
+              ))}
+          </div>
+          <p>Round: {counters.roundCounter}</p>
+        </div>
+      )}
+
+      {counters.roundCounter > 5 && (
+        <div className="container">
+          <div className="resultContainer">
+            <p>{message}</p>
+            <button className="resetButton button" onClick={handleResetClick}>
+              {equations.length > 0 ? "Try again" : "Reset"}
             </button>
-          ))}
-      </div>
-      {counters.roundCounter <= 5 && <p>Round: {counters.roundCounter}</p>}
-
-      <div className="resultContainer">
-        <p>{message} </p>
-        {resetButtonDisplayed && (
-          <button className="resetButton button" onClick={handleResetClick}>
-            Reset
-          </button>
-        )}
-      </div>
-
-      {equationsDisplay && (
-        <div className="equationsContainer">
-          <h4>Your wrong guessess : </h4>
-          {equations
-            .filter((equation) => !equation.isCorrect)
-            .map((equation, index) => (
-              <div key={index} className="equation">
-                <div className="wrongEquation">
-                  <p>
-                    {equation.num1} + {equation.num2} ={" "}
-                    {equation.selectedResult}
-                  </p>
-                  <img src={wrongIcon} alt="Wrong icon" className="wrongIcon" />
-                </div>
-                but should
-                <div className="correctEquation">
-                  <p className="correct">
-                    {equation.num1} + {equation.num2} = {equation.result}
-                  </p>
-                  <img
-                    src={CorrectIcon}
-                    alt="Correct icon"
-                    className="correctIcon"
-                  />
-                </div>
-              </div>
-            ))}
+          </div>
+          {equations.length > 0 && (
+            <div className="wrongEquationsContainer">
+              <h4>Your wrong guesses :</h4>
+              {equations
+                .filter((equation) => !equation.isCorrect)
+                .map((equation, index) => (
+                  <div key={index} className="equation">
+                    <div className="wrongEquation">
+                      <p>
+                        {equation.num1} + {equation.num2} ={" "}
+                        {equation.selectedResult}
+                      </p>
+                      <img
+                        src={wrongIcon}
+                        alt="Wrong icon"
+                        className="wrongIcon"
+                      />
+                    </div>
+                    but should
+                    <div className="correctEquation">
+                      <p className="correct">
+                        {equation.num1} + {equation.num2} = {equation.result}
+                      </p>
+                      <img
+                        src={CorrectIcon}
+                        alt="Correct icon"
+                        className="correctIcon"
+                      />
+                    </div>
+                  </div>
+                ))}
+            </div>
+          )}
         </div>
       )}
     </div>
