6af8882128daa9e834b1bbe860e3a6bd7a62b511
Nama-Salameh <nama.salameh@gmail.com>
2024-04-15 23:49:20 +0300

    Implement countries context task.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import "./App.scss";
-import SummationGameBox from "./summationGameBoxTask/SummationGameBox";
+import CountriesChildComponent from "./CountriesContextAndReducerTask/CountriesChildComponent";
+import CountriesParentComponent from "./CountriesContextAndReducerTask/CountriesParentComponent";
+import { CountriesProvider } from "./contexts/countriesContext/CountriesContext";
 
 function App() {
+  const [isParentVisible, setIsParentVisible] = useState(true);
+
   return (
-    <div className="app">
-      <SummationGameBox />
-    </div>
+    <CountriesProvider>
+      <div className="app">
+        {isParentVisible ? (
+          <CountriesParentComponent
+            goToChild={() => setIsParentVisible(false)}
+          />
+        ) : (
+          <CountriesChildComponent goBack={() => setIsParentVisible(true)} />
+        )}
+      </div>
+    </CountriesProvider>
   );
 }
 
